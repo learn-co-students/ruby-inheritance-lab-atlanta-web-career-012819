fa254cdbb9b2c1732b91c2c051b6b6a9b8c20453
Samidh Patel <spatel1345@gmail.com>
2019-02-21 17:20:32 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+class Student < User
+
+    def initialize(knowledge = nil)
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
+
 
 end
